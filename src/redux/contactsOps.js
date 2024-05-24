dfa40166d4ee36e38793770f91a828b6467f7e00
Artem Lipchansky <lipchanskiy.an@gmail.com>
2024-05-24 22:22:22 +0300

    change

---
+++ src/redux/contactsOps.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+axios.defaults.baseURL = "https://664e1adcfafad45dfadf0b86.mockapi.io/";
+
+export const fetchContacts = createAsyncThunk(
+  "contacts/fetchContacts",
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get("/contacts");
+
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const addContact = createAsyncThunk(
+  "contacts/addContact",
+  async (newContact, thunkAPI) => {
+    try {
+      const response = await axios.post("/contacts", newContact);
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const deleteContact = createAsyncThunk(
+  "contacts/deleteContact",
+  async (id, thunkAPI) => {
+    try {
+      const response = await axios.delete(`/contacts/${id}`);
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
